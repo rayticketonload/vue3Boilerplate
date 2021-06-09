678ee739fcdaecbdd4146c86b223fbfefe03a00f
daisiwei <daisiwei@weychain.com>
2021-06-09 15:56:08 +0800

    vuex config again

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import store from './store'
 import App from './App.vue';
-
 const app = createApp(App);
+
 // vuex 插进来了
 app.use(store);
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
 import { createStore, createLogger } from 'vuex'
+import { ENV_PROD_FLAG } from '/@HELPER/global';
 import profile from './modules/profile';
 import component from './modules/component';
 
-// const debug = process.env.NODE_ENV !== 'production'
+const debug = import.meta.env.MODE !== ENV_PROD_FLAG;
 
 export default createStore({
   modules: {
     profile,
     component
   },
-  // strict: debug,
-  // plugins: debug ? [createLogger()] : []
+  strict: debug,
+  plugins: debug ? [createLogger()] : []
 })
