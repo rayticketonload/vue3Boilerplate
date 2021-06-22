6178401fa001e8ec831b6c0c0aabc1b08c5dccc8
daisiwei <daisiwei@weychain.com>
2021-06-22 11:14:09 +0800

    del shit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+	import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <style>
@@ -12,8 +11,5 @@ import HelloWorld from './components/HelloWorld.vue'
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,6 @@
 <template>
-  <h1>{{ mode }}</h1>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3</a>
-  </p>
-
+	{{ mode }}
   <button type="button" @click="state.count++">count is: {{ state.count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <script>
