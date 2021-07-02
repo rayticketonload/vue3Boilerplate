4b0322c4c253538aa56a96acb8e459065c6a79cf
daisiwei <daisiwei@weychain.com>
2021-07-02 17:32:54 +0800

    lang switch fix

--- src/components/langSelect/index.vue
+++ src/components/langSelect/index.vue
@@ -1,32 +1,75 @@
 <template>
-	<el-dropdown size="mini" split-button type="default">
-		超小尺寸
+	<div class="langSelecter">
+		<el-dropdown @command="handleCommand">
+		<span class="el-dropdown-link">
+			{{ currentLangTag($i18n.locale) }}<i class="el-icon-arrow-down el-icon--right"></i>
+		</span>
 		<template #dropdown>
 			<el-dropdown-menu>
-				<el-dropdown-item>黄金糕</el-dropdown-item>
-				<el-dropdown-item>狮子头</el-dropdown-item>
-				<el-dropdown-item>螺蛳粉</el-dropdown-item>
-				<el-dropdown-item>双皮奶</el-dropdown-item>
-				<el-dropdown-item>蚵仔煎</el-dropdown-item>
+				<el-dropdown-item v-for="(item, index) in langOptions" :command="item.code">
+					{{ item.label }}
+				</el-dropdown-item>
 			</el-dropdown-menu>
 		</template>
 	</el-dropdown>
+	</div>
 </template>
 
 <script>
-// import {  }
+import { LANG_CN_FLAG, LANG_EN_FLAG, LANG_COOKIE_KEY } from '@I18N';
 
 export default {
 	name: 'langSelect',
 
 	data() {
-		return {};
+		return {
+			langOptions: [],
+		};
+	},
+
+	created() {
+		this.setLangOptions();
 	},
 
 	methods: {
+		setLangOptions() {
+			this.langOptions = [
+				{
+					code: LANG_CN_FLAG,
+					label: this.$t('jian-ti-zhong-wen'),
+				},
+				{
+					code: LANG_EN_FLAG,
+					label: this.$t('mei-shi-ying-wen'),
+				}
+			]
+		},
+
+		currentLangTag(lang) {
+			let result = '';
+			this.langOptions.some(item => {
+				if (item.code === lang) {
+					result = item.label;
+				}
+			});
+			return result;
+		},
+
+		handleCommand(command) {
+			this.$i18n.locale = command;
+			localStorage.setItem(LANG_COOKIE_KEY, command);
+			this.setLangOptions();
+		}
 	},
 };
 </script>
 
 <style lang="scss" scoped>
+.langSelecter {
+	padding: $gapSize-public * 2;
+	display: inline-block;
+	vertical-align: middle;
+	box-shadow: 0 0 0 1px $borderColor-primary;
+	@include rounded($borderRadius-public);
+}
 </style>
