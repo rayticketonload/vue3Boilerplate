9171a88ac0973ef6dc1413e3f694ee882a385c0a
daisiwei <daisiwei@weychain.com>
2021-07-07 18:01:03 +0800

    vite env config

--- config/vite.config.js
+++ config/vite.config.js
@@ -20,84 +20,93 @@ function noteProcess(command, mode, modeAfter) {
 	console.log(chalk.blueBright.italic(`  ${warning(`开发模式标签`)} ${mode} ${warning(`置换为`)} ${modeAfterResult}${loading}  ` + '\n'));
 }
 
-const viteDefaultConfig = defineConfig(
-	{
-		plugins: [
-			vue(),
-			// vueI18n({
-			// 	compositionOnly: false,
-			// 	include: path.resolve(__dirname, './path/to/src/locales/**')
-			// })
-		],
-		optimizeDeps: {
-			include: ["axios"]
-		},
-		resolve: {
-			// extensions 选项已经有必须的文件类型参数，导入时想要省略的扩展名列表。注意，vite 不建议忽略自定义导入类型的扩展名（例如：.vue），因为它会干扰 IDE 和类型支持。
-			// 事实上，就算加了 .vue 也不起作用，除非别名的开头用 '/'，例如 '/@VIEWS'，但这样的开头在 style 类型的解释中又不支持
-			alias: [
-				{ find: '@PACKAGE', replacement: '/package.json' },
-				{ find: '@', replacement: '/src' },
-				{ find: '@API', replacement: '/src/api' },
-				{ find: '@ROUTER', replacement: '/src/router' },
-				{ find: '@STORE', replacement: '/src/store' },
-				{ find: '@HELPER', replacement: '/src/helper' },
-				{ find: '@VIEWS', replacement: '/src/views' },
-      	{ find: '@COMPONENTS', replacement: '/src/components' },
-				{ find: '@STYLE', replacement: '/src/style' },
-				{ find: '@ASSETS', replacement: '/src/assets' },
-				{ find: '@I18N', replacement: '/src/i18n' },
-			]
-		},
-		css: {
-			preprocessorOptions: {
-				scss: {
-					additionalData: `@import "@STYLE/global_style";`
-				}
-			}
-		},
-	}
-)
-
+// 接口地图
+// 来自服务器1
+const DOMAIN_ROOT_MAP_1 = {
+	development: 'http://10.10.1.196:8108/',
+	stage: 'http://10.10.1.196:8108/',
+	production: 'http://10.10.1.196:8108/',
+}
+// 来自服务器2
+const DOMAIN_ROOT_MAP_2 = {
+	development: 'https://sx-test.weychain.com:60443/',
+	stage: 'https://sx-test.weychain.com:60443/',
+	production: 'https://sx-test.weychain.com:60443/',
+}
 
 export default ({ command, mode }) => {
 
 	let viteFinalConfig = {};
 
+	// vite默认配置
+	const viteDefaultConfig = defineConfig(
+		{
+			plugins: [
+				vue(),
+				// vueI18n({
+				// 	compositionOnly: false,
+				// 	include: path.resolve(__dirname, './path/to/src/locales/**')
+				// })
+			],
+			optimizeDeps: {
+				include: ["axios"]
+			},
+			resolve: {
+				// extensions 选项已经有必须的文件类型参数，导入时想要省略的扩展名列表。注意，vite 不建议忽略自定义导入类型的扩展名（例如：.vue），因为它会干扰 IDE 和类型支持。
+				// 事实上，就算加了 .vue 也不起作用，除非别名的开头用 '/'，例如 '/@VIEWS'，但这样的开头在 style 类型的解释中又不支持
+				alias: [
+					{ find: '@PACKAGE', replacement: '/package.json' },
+					{ find: '@', replacement: '/src' },
+					{ find: '@API', replacement: '/src/api' },
+					{ find: '@ROUTER', replacement: '/src/router' },
+					{ find: '@STORE', replacement: '/src/store' },
+					{ find: '@HELPER', replacement: '/src/helper' },
+					{ find: '@VIEWS', replacement: '/src/views' },
+					{ find: '@COMPONENTS', replacement: '/src/components' },
+					{ find: '@STYLE', replacement: '/src/style' },
+					{ find: '@ASSETS', replacement: '/src/assets' },
+					{ find: '@I18N', replacement: '/src/i18n' },
+				]
+			},
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: `@import "@STYLE/global_style";`
+					}
+				}
+			},
+		}
+	)
+
 	switch (command) {
 		case 'serve':
-			if (mode === 'development') {
-				noteProcess(
-					command,
-					mode,
-					// ENV_DEV_FLAG
-				);
-				const viteServeDevConfig = defineConfig(
-					{
-						// mode: ENV_DEV_FLAG,
-						server: {
-							cors: true,
-          		open: true,
-							proxy: {
-								'/testapi': {
-									target: 'http://10.10.1.196:8108/',
-									changeOrigin: true,
-									rewrite: path => path.replace(/^\/testapi/, '')
-								},
-								'/pms': {
-									target: 'https://sx-test.weychain.com:60443/',
-									changeOrigin: true,
-								}
+
+			const viteServeConfig = defineConfig(
+				{
+					server: {
+						cors: true,
+						open: true,
+						proxy: {
+							'/testapi': {
+								target: DOMAIN_ROOT_MAP_1[mode],
+								changeOrigin: true,
+								rewrite: path => path.replace(/^\/testapi/, '')
+							},
+							'/pms': {
+								target: DOMAIN_ROOT_MAP_2[mode],
+								changeOrigin: true,
 							}
 						}
 					}
-				)
-				Object.assign(viteFinalConfig, viteDefaultConfig, viteServeDevConfig);
-			}
+				}
+			)
+			Object.assign(viteFinalConfig, viteDefaultConfig, viteServeConfig);
 			return viteFinalConfig;
+
 		case 'build':
 			noteProcess(command, mode);
 			return viteDefaultConfig;
+
 		case 'optimize':
 			break;
 			default:
--- src/api/config.js
+++ src/api/config.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-// import { LANG_CN_FLAG } from '@HELPER/global';
+import { i18n } from '@I18N';
 
 export default {
 	use() {
 		axios.defaults.timeout = 10000;
 		axios.defaults.headers.post['Content-Type'] = 'application/json';
-		// axios.defaults.headers.lang = LANG_CN_FLAG; // 后端的返回数据涉及的语种变更应该从浏览器本地存储来决定
+		axios.defaults.headers.lang = i18n.global.locale;
 	},
 };
